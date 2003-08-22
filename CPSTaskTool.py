d7f0b4a6014df7ae0d26e1c5b2a7b967d492f2bf
janguenot <devnull@localhost>
2003-08-22 15:40:26 +0000

    Task tool

---
+++ CPSTaskTool.py
@@ -0,0 +1,36 @@
+# (c) 2002 Nuxeo SARL <http://nuxeo.com>
+# (c) 2003 CEA <http://www.cea.fr>
+# Author: Julien Anguenot <ja@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+# $Id$
+
+"""
+    CPS Task Tool
+"""
+
+from Globals import InitializeClass
+from Products.CMFCore.utils import UniqueObject
+
+class CPSTaskTool(UniqueObject):
+    """
+        Provides Task Repository
+    """
+
+    id = 'portal_tasks'
+    meta_type = 'CPS Task Tool'
+
+InitializeClass(CPSTaskTool)
