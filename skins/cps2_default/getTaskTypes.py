447a9a07dc9570387f04608e9ba348726afad5cf
janguenot <devnull@localhost>
2003-10-13 08:48:01 +0000

    For customization purposes : give the possible types for a task

---
+++ skins/cps2_default/getTaskTypes.py
@@ -0,0 +1,17 @@
+## Script (Python) "getTaskTypes.py"
+##parameters
+#$Id$
+
+"""
+Return the different task types.
+It has to be customized according to your needs.
+"""
+
+task_types = ['Administrative Business',
+              'Atomic Business',
+              'Internal Business',
+              'Some other business',
+              'Nuxeo Business',
+              ]
+
+return task_types
