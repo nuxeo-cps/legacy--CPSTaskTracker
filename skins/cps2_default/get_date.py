e70ab880d6d9ec71073ec014cfdedeace731221c
janguenot <devnull@localhost>
2003-10-13 13:19:28 +0000

    Return the localized date

---
+++ skins/cps2_default/get_date.py
@@ -0,0 +1,35 @@
+## Script (Python) "get_date.py"
+##parameters=cdate=None, id=None, fmt=None
+#$Id$
+
+"""
+Return the date according to the current language
+"""
+
+
+from DateTime import DateTime
+from re import match
+
+fmt = "%m/%d/%Y"
+lang = context.portal_messages.get_selected_language()
+
+if cdate and match(r'^[0-9]?[0-9]/[0-9]?[0-9]/[0-9]{4,4}$', cdate):
+    if lang == 'fr':
+        d, m, y = cdate.split('/')
+        fmt = "%d/%m/%Y"
+    else:
+        m, d, y = cdate.split('/')
+        fmt = "%m/%d/%Y"
+    try:
+        dtv = DateTime(int(y), int(m), int(d), 0, 0)
+    except:
+        dtv = DateTime()
+else:
+    dtv = DateTime()
+    if lang == 'fr':
+        fmt = "%d/%m/%Y"
+
+return {'day':dtv.day(),
+        'month':dtv.month(),
+        'year':dtv.year(),
+        'all':dtv.strftime(fmt)}
