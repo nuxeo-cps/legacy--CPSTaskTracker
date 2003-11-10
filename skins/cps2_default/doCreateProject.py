f1e755950ee2c2d61a0f4bcd208286e35457b7ce
janguenot <devnull@localhost>
2003-11-10 09:28:50 +0000

    Beginning : skins for project management

---
+++ skins/cps2_default/doCreateProject.py
@@ -0,0 +1,24 @@
+## Script (Python) "doCreateProject"
+##parameters=REQUEST=None
+
+"""
+Create a new project
+"""
+
+#
+# XXX : not finished
+#
+
+if REQUEST is not None and \
+   REQUEST.form is not None:
+    form = REQUEST.form
+    title = form.get("title", "")
+    description = form.get("description", "")
+    if title != []:
+        new_project = {}
+        new_project['title'] = title
+        new_project['description'] = description
+        context.portal_tasks.addProject(new_project)
+
+
+return context.REQUEST.RESPONSE.redirect(context.absolute_url())
