b0c564c7b5a13d935ed685cd42a1d0b4e2f2ea8d
janguenot <devnull@localhost>
2003-11-12 02:40:55 +0000

    Spliting the edit/edit_parameters views

--- skins/cps2_default/cps_task_edit.py
+++ skins/cps2_default/cps_task_edit.py
@@ -19,19 +19,20 @@ if compute_dateiso is not None and date is not None:
     kw['dateiso'] = dateiso
     del kw['compute_dateiso']
 
-# Dates localized
 
-start_date = kw.get('start_task_date', None)
-if start_date is not None:
-    kw['start_task_date'] = context.get_date(start_date)['for_storing']
+if kw.get('parameters'):
+    # Dates localized
+    start_date = kw.get('start_task_date', None)
+    if start_date is not None:
+        kw['start_task_date'] = context.get_date(start_date)['for_storing']
 
-stop_date = kw.get('stop_task_date', None)
-if stop_date is not None:
-    kw['stop_task_date'] = context.get_date(stop_date)['for_storing']
+    stop_date = kw.get('stop_task_date', None)
+    if stop_date is not None:
+        kw['stop_task_date'] = context.get_date(stop_date)['for_storing']
 
 here = context.this()
 here.edit(**kw)
 
 if REQUEST is not None:
     psm = 'portal_status_message='+context.portal_messages('_cpsdocuments_Document_modified_')
-    REQUEST.RESPONSE.redirect('%s/view?%s' % (here.absolute_url(), psm))
+    REQUEST.RESPONSE.redirect('%s?%s' % (here.absolute_url()+"/cps_task_edit_parameters_form", psm))
