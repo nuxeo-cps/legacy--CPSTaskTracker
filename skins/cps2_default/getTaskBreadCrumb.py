f147ee72866d8ce43b77c8f07de1a78ad349b283
janguenot <devnull@localhost>
2003-11-17 11:02:21 +0000

    Handling case where the task is just created. Updating the title in the breadcrumb

--- skins/cps2_default/getTaskBreadCrumb.py
+++ skins/cps2_default/getTaskBreadCrumb.py
@@ -20,7 +20,12 @@ else:
         if bc is not None:
             stupid = 0
             for item in bc:
-                if item['title'] == context.title_or_id():
+                if item['url'] == context.absolute_url():
+                    #
+                    # Here the title could have change.
+                    # When you we just create a task.
+                    #
+                    item['title'] = context.title_or_id()
                     stupid = 1
             if stupid == 0:
                 del bc[0]
