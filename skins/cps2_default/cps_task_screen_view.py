56b5176961d5b84c718733705f4700e46a07ea59
janguenot <devnull@localhost>
2003-11-18 21:53:57 +0000

    More stable like that

--- skins/cps2_default/cps_task_screen_view.py
+++ skins/cps2_default/cps_task_screen_view.py
@@ -8,7 +8,7 @@ Return the skinner for the task screen.
 Rendering it according to the screener.
 """
 
-#render_template = "cps_task_screen_view_template"
+render_template = "cps_task_screen_view_template"
 all_skinners = context.getTaskScreenSkinners()
 
 for skin in all_skinners:
